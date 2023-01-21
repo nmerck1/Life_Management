af640fd5bc906321e6865ccd2cb5383263d2588e
nmerck1 <nathanielmerck@yahoo.com>
2023-01-21 01:16:52 -0500

    Updated version in footer

--- classes/footer.class.php
+++ classes/footer.class.php
@@ -4,7 +4,7 @@
 class Footer {
 
   public function show_footer(){
-    $current_version = '1.33.49.15';
+    $current_version = '1.33.50.15';
 
     echo '<footer class="container-fluid text-center">';
       echo '<p class="bi-egg-fill" style="color:white;"></p>';
